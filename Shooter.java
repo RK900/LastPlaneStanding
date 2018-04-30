cf99abd7dea28566a7d1f5a7fb3e92548a5c7c1c
ravigupta384923 <ravigupta384923@gmail.com>
2018-04-30 09:21:22 -0700

    HI

--- Shooter.java
+++ Shooter.java
@@ -1,4 +1,18 @@
-class Shooter
-{
+package LastPlaneStanding;
 
+public class Shooter
+{
+    public Shooter( int x, int y )
+    {
+        
+    }
+    public void move()
+    {
+        
+    }
+    public void shoot( Projectile p )
+    {
+        
+    }
+    
 }
