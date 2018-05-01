ca85fe54cddbb0ba359e80befeae4e23c35bcb0e
dxu12 <dxu889@student.fuhsd.org>
2018-05-01 08:39:29 -0700

    I changed Tank.java

--- Tank.java
+++ Tank.java
@@ -1,14 +1,25 @@
 package LastPlaneStanding;
 
+import java.awt.Image;
+
 class Tank extends Firer
 {
+    private double x, y, vx, vy = 0;
+    private Image image1, image2, deadImage;
+    private double width, height;
+    private boolean markedDead, isDead, toBeRemoved;
 
     public Tank() 
     {
         
     }
     
-    public boolean fire()
+    public void move()
+    {
+        
+    }
+    
+    public void fire()
     {
         
     }
