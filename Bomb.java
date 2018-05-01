688b3a98a87042ccab4de656b343bb29b57b01d5
ravigupta384923 <ravigupta384923@gmail.com>
2018-05-01 09:00:30 -0700

    Most classes changes

--- Bomb.java
+++ Bomb.java
@@ -1,4 +1,28 @@
-class Bomb extends Projectile implements Gravity
+package LastPlaneStanding;
+
+public class Bomb extends Projectile
 {
+    private int damage = 100;
+    private int x;
+    private int y;
+    private boolean intact;
+
+    public Bomb( int x, int y )
+    {
+        super( x, y );
+        this.x = x;
+        this.y = y;
+        super.damage = this.damage;
+        intact = true;
+        
+    }
+    public void move()
+    {
+        
+    }
+    public void collide() 
+    {
+        
+    }
 
 }
