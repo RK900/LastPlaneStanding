735651d2496809356f0adb4eff0b8893d4a73f74
ravigupta384923 <ravigupta384923@gmail.com>
2018-05-04 09:11:56 -0700

    Messsage

--- Plane.java
+++ Plane.java
@@ -1,23 +1,44 @@
 package LastPlaneStanding;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Plane extends Shooter
 {
     private int health;
-    private int x;
-    private int y;
-    private int cx;
-    private int cy;
+    public int x, y, cx, cy, width, height;
+    public Image img;
     public Plane( int health, int x, int y )
     {
         super(health, x, y);
         health = 10;
         cx = x;
         cy = y;
-        
+        width = 10;
+        height = 10;
+        try {
+            img = ImageIO.read(new File("plane.jpg"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void update(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawImage(img, x, y, width, height, null);
     }
     public void move()
     {
        
+    }
+    public void reset()
+    {
+        
     }
     public int getX()
     {
