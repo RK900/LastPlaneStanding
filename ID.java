a86393412f1f158e1780cd9e5675c0fa2f01ce04
Rohan Koodli <rovik05@gmail.com>
2018-05-08 09:09:09 -0700

    windows

---
+++ ID.java
@@ -0,0 +1,11 @@
+package LastPlaneStanding;
+
+public enum ID {
+    Player(),
+    Enemy(),
+    Tank(),
+    Plane(),
+    EnemyPlane(),
+    SmartTank(),
+    SuperTank();
+}
---
+++ Player.java
@@ -0,0 +1,19 @@
+package LastPlaneStanding;
+
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(x, y, 32, 32);
+    }
+}
