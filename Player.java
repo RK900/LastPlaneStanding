f27e96e4ce4b91074a605bf7ffb2d36554c2295c
ravigupta384923 <ravigupta384923@gmail.com>
2018-05-15 08:41:42 -0700

    Basic bullet and Player plane

--- Player.java
+++ Player.java
@@ -4,18 +4,24 @@ import java.awt.*;
 
 public class Player extends GameObject {
 
+    private int width, height;
     public Player(int x, int y, ID id) {
         super(x, y, id);
+        width = 30;
+        height = 30;
+        
     }
 
     public void tick() {
         x += velX;
         y += velY;
+        x = Game.clamp( x, 0, Game.WIDTH - width );
+        y = Game.clamp( y, 0, Game.HEIGHT - height * 2 );
     }
 
     public void render(Graphics g) {
         g.setColor(Color.white);
-        g.fillRect(x, y, 32, 32);
+        g.fillRect(x, y, width, height);
     }
 
     @Override
