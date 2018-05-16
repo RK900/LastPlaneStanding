f697312412ab923f0a0b841dba8ca04fb4df1f09
dxu12 <dxu889@student.fuhsd.org>
2018-05-16 10:26:51 -0700

    updated SmartTank

--- SmartTank.java
+++ SmartTank.java
@@ -1,6 +1,10 @@
 package LastPlaneStanding;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,38 +12,61 @@ import javax.imageio.ImageIO;
 
 public class SmartTank extends Tank
 {
-    private static final int ACCURACY = 5;
-    private static final int SPEED = 5;
-    private int x, y, cx, cy = 0;
-    public Image img;
-    
-    public SmartTank(int health, int x, int y) 
+    private Image img;
+
+    private int accY;
+    private float alpha = 1;
+    private float life;
+    private Handler handler;
+    public SmartTank( int x, int y, ID id, float life, Handler handler )
     {
-        super(8, x, y, ACCURACY, SPEED);
-        cx = x;
-        cy = y;
+        super( x, y, id, life, handler );
+        width = 30;
+        height = 30;
+        this.life = life;
+        this.handler = handler;
+        accY = 1;
+        velX = 4;
         try
         {
-            img = ImageIO.read( new File( "SmartTank.png" ) );
+            img = ImageIO.read( new File( "bomb.png" ) );
         }
         catch ( IOException e )
         {
             e.printStackTrace();
         }
     }
-    
-    public void move()
+
+
+    public void tick()
     {
-        
+//        if( alpha > life)
+//        {
+//            alpha -= (life - 0.001f);
+//        }else handler.removeObject( this );
+        x += velX;
+        //velY += accY;
+        if ( x <= 0 || x >= Game.WIDTH - 16) {
+          velX *= -1;
+      }    }
+
+
+    public void render( Graphics g )
+    {
+//        Graphics2D g2d = (Graphics2D) g;
+//        g2d.setComposite( makeTransparent( alpha  ) );
+        g.setColor( Color.blue );
+        g.fillRect( x, y, width, height );
+        //g.drawImage( img, x, y, width, height, null );
+//        g2d.setComposite( makeTransparent( 1 ));
     }
-    
-    public int getX()
+    private AlphaComposite makeTransparent( float alpha )
     {
-        return cx;
+        int type = AlphaComposite.SRC_OVER;
+        return AlphaComposite.getInstance( type, alpha );
     }
-    
-    public int getY()
+    public Rectangle getBounds()
     {
-        return cy;
+        return new Rectangle(x, y, width, height);
     }
 }
