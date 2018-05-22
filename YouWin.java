6bb1515680b99db6852c77d5168fb41a9af8fb2c
Rohan Koodli <rovik05@gmail.com>
2018-05-22 08:48:41 -0700

    Update YouWin.java

--- YouWin.java
+++ YouWin.java
@@ -1,41 +1,21 @@
 package LastPlaneStanding;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.ImageObserver;
 
-/**
-*
-*  Prints out gameOver or youWin
-*
-*  @author  Rohan
-*  @version May 20, 2018
-*  @author  Period: 2
-*  @author  Assignment: APCSFinal
-*
-*  @author  Sources: Ravi, Rohan, David
-*/
-
 public class YouWin extends GameObject
 {
     private String s;
-    
-    /**
-     * Constructor of YouWin
-     * @param x
-     * @param y
-     * @param id
-     * @param s
-     */
+    private Font font;
     public YouWin(int x, int y, ID id, String s) {
         super(x, y, id);
+        font = new Font( "SansSerif", Font.PLAIN, 36 );
         this.s = s;
     }
 
-    /**
-     * Updates win
-     */
     @Override
     public void tick()
     {
@@ -43,24 +23,16 @@ public class YouWin extends GameObject
         
     }
 
-    /**
-     * Renders winning
-     *
-     */
     @Override
     public void render( Graphics g )
     {
+        g.setFont( font );
         g.setColor( Color.white );
         g.drawString( s, x, y );
         Game.lose = true;
         
     }
 
-    /**
-     * gets bounds of screen
-     *
-     * @return Rectangle with screen bounds
-     */
     @Override
     public Rectangle getBounds()
     {
